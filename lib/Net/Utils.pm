d4736d05793171664b5333748df8faa582131a96
infrared <infrared@pickle.(none)>
2012-04-09 07:54:03 -0400

    stuff

--- lib/Net/Utils.pm
+++ lib/Net/Utils.pm
@@ -46,32 +46,21 @@ Returns network address of a given IP & mask
 
 sub network {
         my ($self,$ip,$mask) = @_;
-        
-=cut
-        # TO DO
-        
-        # mask in binary
-        my $ones = $prefix;
-        my $zeros = 32 - $prefix;
-        my $binary_mask = 1 x $ones . 0 x $zeros;
+        my $binary_mask = $self->valid_mask($mask);
+
         my @mask = split('',$binary_mask);
 
         # IP in binary
+        my @octets = map{ dec2bin($_) } split('\.',$ip);
         my $join = join('',@octets);
         my @ipaddr = split('',$join);
 
 
         my @network = pairwise { $a & $b } @mask,@ipaddr;
-        my $network_binary = join('',@network);
-        my @network_octets = ($network_binary =~ /(\d{8})/g);
-        my @decimals = map{ _bin2dec($_) } @network_octets;
+        return join ('', @network);
+
         
-        my $question = "Given $ip/$prefix, network address is?";
-        my $answer = join('.',@decimals);
-        
-        my @array = ($question,$answer);
-        return @array;
-=cut
+
 }
 
 =head2 broadcast
@@ -80,12 +69,34 @@ Returns broadcast address of a given IP & mask
 
 =cut
 
+sub broadcast {
+        my ($self,$ip,$mask) = @_;
+        my $binary_mask = $self->valid_mask($mask);
+
+        my @mask = split('',$binary_mask);
+
+        # IP in binary
+        my @octets = map{ dec2bin($_) } split('\.',$ip);
+        my $join = join('',@octets);
+        my @ipaddr = split('',$join);
+
+
+        my @broadcast = pairwise { $a | $b } @mask,@ipaddr;
+        my $broadcast_binary = join('',@broadcast);
+
+        return $self->binary_to_ip($broadcast_binary);
+
+}
+
 =head2 random_ip
 
 Generate random IP address
 
 =cut
-sub _randomip {
+sub randomip {
+        # TO DO
+        # Class A,B,C, private/public
+        
         my $ip = join ".", map int rand 256, 1 .. 4;
         return $ip;
 }
@@ -96,7 +107,7 @@ Convert binary to decimal
 
 =cut
 
-sub _bin2dec {
+sub bin2dec {
         my $binary = shift;
         my $decimal = unpack("N", pack("B32", substr("0" x 32 . $binary, -32)));
         return $decimal;
@@ -127,33 +138,36 @@ sub dec2bin_nibble {
         return $binary;
 }
 
-=head2 valid_mask
+=head2 valid_mask_binary
 
-Validate a netmask
+Validates a netmask, and returns the mask binary format
 
 =cut
 sub valid_mask {
     my ($self,$mask) = @_;
     
      if ($mask =~ /^\/\d{1,2}$/) {
-        # TO DO CIDR stuff
-=cut
-        my $cidr
-        my $ones = $prefix;
-        my $zeros = 32 - $prefix;
-        my $binary_mask = 1 x $ones . 0 x $zeros;
-        my @mask = split('',$binary_mask);
         # CIDR
-=cut    
+
+        my ($ones)  = ($mask =~ /^\/(\d{1,2})$/);  # a /30 is 30 1's
+        my $zeros = 32 - $ones;
+        my $binary_mask = 1 x $ones . 0 x $zeros;
+        if ($binary_mask =~ /^1+0*$/) {
+            return $binary_mask;
+        }
+        else {
+            return 0;
+        }
+
+
     }
     elsif ($mask =~ /^\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/) {
         my (@octets) = ($mask =~ /(\d+)/g);
              
         my @binary_octets = map{ dec2bin($_) } @octets;
         my $binary_mask = join('',@binary_octets);
-        print $binary_mask,"\n";
-        if ($binary_mask =~ /^1+0+?$/) {
-            return "valid";
+        if ($binary_mask =~ /^1+0*$/) {
+            return $binary_mask;
         }
         else {
             return 0;
@@ -176,6 +190,20 @@ sub valid_ip {
     return; 
 }
 
+=head2 binary_to_ip {
+
+Convert 32 bit binary into an IP address format
+
+=cut
+
+sub binary_to_ip {
+    my ($self,$binary) = @_;
+    my @octets = ($binary =~ /(\d{8})/g);
+    my @decimals = map{ bin2dec($_) } @octets;
+        
+        
+    return join('.',@decimals);
+}
 
 
 
--- test.pl
+++ test.pl
@@ -7,3 +7,8 @@ use Net::Utils;
 my $nu = Net::Utils->new;
 
 print $nu->valid_mask('255.128.0.0');
+
+print "\n";
+print $nu->valid_mask('/32');
+
+print $nu->network('192.168.10.56','/24')->binary_to_ip;
